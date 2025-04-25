6b0a3b62f10066950960cb0f1233e294ddaf2853
shashank-pd <shashankpd2606@gmail.com>
2025-04-25 18:27:47 +0530

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    content: ['./src/**/*.{js,ts,jsx,tsx,html}'],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}', // If you're using the new app directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
